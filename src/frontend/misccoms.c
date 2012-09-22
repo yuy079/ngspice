7b3eac8a8154ef2627b0ba793f5ca7638e47c82a
rlar <rlar>
2012-09-22 10:53:05 +0200

    invoke cleanup functions in com_quit()
    trying to get the valgrind reports down.

--- src/frontend/misccoms.c
+++ src/frontend/misccoms.c
@@ -16,6 +16,8 @@ Author: 1985 Wayne A. Christopher, U. C. Berkeley CAD Group
 #include "plotting/graf.h"
 #include "display.h"
 #include "../misc/ivars.h"
+#include "com_alias.h"
+#include "define.h"
 
 #ifdef HAVE_GNUREADLINE
 #include <readline/readline.h>
@@ -56,9 +58,19 @@ com_quit(wordlist *wl)
     /* start to clean up the mess */
 
     {
-        wordlist *t = wl_cons(copy("all"), NULL);
-        com_destroy(t);
-        wl_free(t);
+        wordlist all = { "all", NULL, NULL };
+        wordlist star = { "*", NULL, NULL };
+
+        com_destroy(&all);
+        com_unalias(&star);
+        com_undefine(&star);
+
+        cp_remvar("history");
+        cp_remvar("noglob");
+        cp_remvar("brief");
+        cp_remvar("sourcepath");
+        cp_remvar("program");
+        cp_remvar("prompt");
     }
 
 #ifdef EXPERIMENTAL_CODE
