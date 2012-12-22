2351fc8af3961400e29acb13a509c5b19a109cd4
h_vogt <h_vogt>
2012-12-22 17:57:31 +0100

    vsrc, isrc TRNOISE: allow repeated calls to 'tran'

--- src/spicelib/devices/vsrc/vsrcload.c
+++ src/spicelib/devices/vsrc/vsrcload.c
@@ -345,11 +345,16 @@ VNoi3 3 0  DC 0 TRNOISE(0 0 0 0 15m 22u 50u) : generate RTS noise
                         double TS = state -> TS;
                         double RTSAM = state->RTSAM;
 
+                        /* reset top (hack for repeated tran commands) */
+                        if (time == 0)
+	                        state->top = 0;
+
                         /* no noise */
                         if(TS == 0.0) {
                             value = 0.0;
                         } else {
-                            /* 1/f and white noise */
+							
+							/* 1/f and white noise */
                             size_t n1 = (size_t) floor(time / TS);
 
                             double V1 = trnoise_state_get(state, ckt, n1);
@@ -368,6 +373,7 @@ VNoi3 3 0  DC 0 TRNOISE(0 0 0 0 15m 22u 50u) : generate RTS noise
                         /* DC value */
                         if(here -> VSRCdcGiven)
                             value += here->VSRCdcValue;
+
                     }
                     break;
 
