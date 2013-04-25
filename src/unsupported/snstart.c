3255db8b7ea2ae612dda27e816b6fe0dd9e4d127
dwarning <dwarning>
2013-04-25 21:15:56 +0200

    adapt the sense2 code to ngspice

--- src/unsupported/snstart.c
+++ src/unsupported/snstart.c
@@ -24,10 +24,12 @@ SENstartup(CKTcircuit *ckt, int restart)
     int type;
     GENinstance *fast;
 
+/*
     if (restart) {
         fprintf(stderr, "unsupported code, (SENstartup), doesn't support restart\n");
         exit(1);
     }
+*/
 
 #ifdef SENSDEBUG
     printf("SENstartup\n");
@@ -38,6 +40,7 @@ SENstartup(CKTcircuit *ckt, int restart)
     ckt->CKTsenInfo->SENinitflag = ON; /* allocate memory in NIsenReinit */
 
     parmtemp.iValue = 1;
+    parmtemp.rValue = 1.0;
 
     for (i = 0; i < ckt->CKTsenInfo->SENnumVal; i++) {
         type = -1;
@@ -49,6 +52,10 @@ SENstartup(CKTcircuit *ckt, int restart)
         if (err != OK)
             return err;
 
+#ifdef SENSDEBUG
+        printf("SENstartup Instance: %s Design parameter: %s\n", ckt->CKTsenInfo->SENdevices[i],
+                                                                 ckt->CKTsenInfo->SENparmNames[i]);
+#endif
         err = CKTpName(
             ckt->CKTsenInfo->SENparmNames[i],
             &parmtemp, ckt, type,
