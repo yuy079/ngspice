fdf64f15ec6b18d8069a549c97d0ce4c245680e7
rlar <rlar>
2013-07-06 17:40:29 +0200

    CKTfndMod(), drop unused arguments `type' and `modfast'

--- src/frontend/spiceif.c
+++ src/frontend/spiceif.c
@@ -621,7 +621,7 @@ finddev_special(
 
     // assert(second && *second == -1)
     // assert(third && *third == NULL)
-    *modptr = ft_sim->findModel (ckt, -1, NULL, name);
+    *modptr = ft_sim->findModel (ckt, name);
     if (*modptr) {
         *device_or_model = 1;
         return (*modptr)->GENmodType;
@@ -1217,7 +1217,7 @@ finddev(CKTcircuit *ckt, char *name, GENinstance **devptr, GENmodel **modptr)
 
     // assert(second && *second == -1)
     // assert(third && *third == NULL)
-    *modptr = ft_sim->findModel (ckt, -1, modptr, name);
+    *modptr = ft_sim->findModel (ckt, name);
     if (*modptr)
         return (*modptr)->GENmodType;
 
