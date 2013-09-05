2f455bfb463acf87242ba30bf06e12f97c25284e
h_vogt <h_vogt>
2013-09-05 12:00:05 +0200

    Revert "inpcom.c: remove memory leak during setup"

--- src/frontend/inpcom.c
+++ src/frontend/inpcom.c
@@ -5945,7 +5945,7 @@ subckt_params_to_param(struct line *card)
     for (; card; card = card->li_next) {
         char *curr_line = card->li_line;
         if (ciprefix(".subckt", curr_line)) {
-            char *cut_line, *new_line, *new_li_line;
+            char *cut_line, *new_line;
             cut_line = strstr(curr_line, "params:");
             if (!cut_line)
                 continue;
@@ -5954,9 +5954,7 @@ subckt_params_to_param(struct line *card)
             /* replace "params:" by ".param " */
             memcpy(new_line, ".param ", 7);
             /* card->li_line ends with subcircuit name */
-            new_li_line = copy_substring(curr_line, --cut_line);
-            tfree(card->li_line);
-            card->li_line = new_li_line;
+            cut_line[-1] = '\0';
             /* insert new_line after card->li_line */
             card->li_next = xx_new_line(card->li_next, new_line,
                                         card->li_linenum + 1, 0);
