e3e16f05ad55584fe56f4fe1f2fe305414b6bd7c
dwarning <dwarning>
2013-10-28 19:39:35 +0100

    correct the voltage sign for in printout

--- src/spicelib/devices/dio/diosoachk.c
+++ src/spicelib/devices/dio/diosoachk.c
@@ -38,7 +38,7 @@ DIOsoaCheck(CKTcircuit *ckt, GENmodel *inModel)
             if (vd > model->DIOfv_max)
                 if (warns_fv < maxwarns_fv) {
                     soa_printf(ckt, (GENinstance*) here,
-                               "Vf=%g has exceeded Fv_max=%g\n",
+                               "Vj=%g has exceeded Fv_max=%g\n",
                                vd, model->DIOfv_max);
                     warns_fv++;
                 }
@@ -46,8 +46,8 @@ DIOsoaCheck(CKTcircuit *ckt, GENmodel *inModel)
             if (-vd > model->DIObv_max)
                 if (warns_bv < maxwarns_bv) {
                     soa_printf(ckt, (GENinstance*) here,
-                               "|Vj|=%g has exceeded Bv_max=%g\n",
-                               -vd, model->DIObv_max);
+                               "Vj=%g has exceeded Bv_max=%g\n",
+                               vd, model->DIObv_max);
                     warns_bv++;
                 }
 
