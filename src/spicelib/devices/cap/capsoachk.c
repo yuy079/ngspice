ab80f7986b6d835b7c7277d804ed25a30e1398b8
dwarning <dwarning>
2013-10-30 20:08:57 +0100

    devices/cap, implement CAPsoaCheck()

---
+++ src/spicelib/devices/cap/capsoachk.c
@@ -0,0 +1,46 @@
+/**********
+Copyright 1990 Regents of the University of California.  All rights reserved.
+Author: 1985 Thomas L. Quarles
+**********/
+
+#include "ngspice/ngspice.h"
+#include "ngspice/cktdefs.h"
+#include "capdefs.h"
+#include "ngspice/trandefs.h"
+#include "ngspice/sperror.h"
+#include "ngspice/suffix.h"
+#include "ngspice/cpdefs.h"
+
+
+int
+CAPsoaCheck(CKTcircuit *ckt, GENmodel *inModel)
+{
+    CAPmodel *model = (CAPmodel *) inModel;
+    CAPinstance *here;
+    double vc;  /* current capacitor voltage */
+    int maxwarns_bv = 0;
+    static int warns_bv = 0;
+
+    for (; model; model = model->CAPnextModel) {
+
+        maxwarns_bv = ckt->CKTsoaMaxWarns;
+
+        for (here = model->CAPinstances; here; here = here->CAPnextInstance) {
+
+            vc = fabs(ckt->CKTrhsOld [here->CAPposNode] -
+                      ckt->CKTrhsOld [here->CAPnegNode]);
+
+            if (vc > here->CAPbv_max)
+                if (warns_bv < maxwarns_bv) {
+                    soa_printf(ckt, (GENinstance*) here,
+                               "|Vc|=%g has exceeded Bv_max=%g\n",
+                               vc, here->CAPbv_max);
+                    warns_bv++;
+                }
+
+        }
+
+    }
+
+    return OK;
+}
