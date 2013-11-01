75b574a77cc074b3654717209ce22299d36717aa
rlar <rlar>
2013-11-01 20:15:21 +0100

    introduce CKTsoaInit()

--- src/spicelib/devices/cktsoachk.c
+++ src/spicelib/devices/cktsoachk.c
@@ -13,6 +13,21 @@
 
 
 int
+CKTsoaInit(void)
+{
+    int i;
+
+    SPICEdev **devs = devices();
+
+    for (i = 0; i < DEVmaxnum; i++)
+        if (devs[i] && devs[i]->DEVsoaCheck)
+            devs[i]->DEVsoaCheck (NULL, NULL);
+
+    return OK;
+}
+
+
+int
 CKTsoaCheck(CKTcircuit *ckt)
 {
     int i, error;
